77e17a66fbcd8420553dd1bfa428b8ea8ffe6f65
Gagan Khanna <gagan.khanna@avanade.com>
2024-03-07 14:38:56 -0700

    Add FizBuzz Logic #1

---
+++ DivisionLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzz
+{
+    public static class DivisionLogger
+    {
+        public static void LogDivision(int number, int divisor)
+        {
+            Console.WriteLine($"Divided {number} by {divisor}");
+        }
+
+        public static void LogInvalidItem()
+        {
+            Console.WriteLine("Invalid Item");
+        }
+    }
+}
